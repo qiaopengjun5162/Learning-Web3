24b2d1241ba36a280e5b3bba7d9a1ad41f66e0b6
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-10-01 14:27:23 +0800

    feat: simple uniswap2

---
+++ simple_uniswap2/src/Pair.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Pair {
+    address public factory; // 工厂合约地址
+    address public token0; // 代币1
+    address public token1; // 代币2
+
+    constructor() payable {
+        factory = msg.sender;
+    }
+
+    // called once by the factory at time of deployment
+    function initialize(address _token0, address _token1) external {
+        require(msg.sender == factory, "UniswapV2: FORBIDDEN"); // sufficient check
+        token0 = _token0;
+        token1 = _token1;
+    }
+}
